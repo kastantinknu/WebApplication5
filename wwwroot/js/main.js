a6ec7c23617f416b71dc222de05be2dc2ae5c3f5
Kostyantyn Zabolotnyy <kastantin.knu@gmail.com>
2021-06-11 15:56:48 +0300

    Добавьте файлы проекта.

---
+++ wwwroot/js/main.js
@@ -0,0 +1,46 @@
+﻿$(document).ready(function () {
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+
+    $(".zoom").hover(function () {
+
+        $(this).addClass('transition');
+    }, function () {
+
+        $(this).removeClass('transition');
+    });
+});
+//$('a.accordion-toggle').click(function () {
+
+//        $(this).text('Отправлено');
+//        $(collapseOne1).text($(btnGroupDrop1).text());
+//    //if ($(this).next('.accordion-body').hasClass('in')) {
+
+
+//    //} else {
+//    //    $(this).text('Отправлено');
+//    //    $(collapseOne1).text($(btnGroupDrop1).text());
+//    //}
+
+//    //if ($(this).next('.accordion-body').hasClass('in')) {
+//    //    $(this).text('Отправлено');
+//    //} else {
+//    //    $(this).text('Find Cafe');
+//    //}
+//});
+$('a.dropdown-item').click(function () {
+
+
+
+    $(btnGroupDrop1).text($(this).text());
+
+});
+
+$(dislikes).click(function () {
+
+
+    $(ressend).text("Отправлено "+$(btnGroupDrop1).text());
+
+});
